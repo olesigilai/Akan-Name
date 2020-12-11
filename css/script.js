b9dee95b801096a010924b9146f4423ec83292a8
olesigilai <ole_sigilai@yahoo.com>
2020-12-11 14:44:58 +0300

    changes made

--- css/script.js
+++ css/script.js
@@ -1,2 +1,6 @@
 var maleName =["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw"]
 var femaleName =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+function getDay (cc,yy,mm,dd)
+{
+return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7
+}
