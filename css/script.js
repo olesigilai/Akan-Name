1fd0acdc99e5837293d1faf98121ddd5df876470
olesigilai <ole_sigilai@yahoo.com>
2020-12-11 16:24:18 +0300

    added functions

--- css/script.js
+++ css/script.js
@@ -1,6 +1,22 @@
 var maleName =["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw"]
 var femaleName =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-function getDay (cc,yy,mm,dd)
-{
-return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7
+function getDay (){
+
+
+var year = document.getElementById("year").value ;
+
+var mm =parseInt(document.getElementById("month").value);
+
+var day = parseInt(document.getElementById("day").value);
+
+var gender = (document.getElementById("gender"));
+
+
+var yy = parseInt(year.substr(2,4));
+
+
+var century = parseInt(year.substr(0,2));
+
+var date = parseInt( ( (century/4) -2*century-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + day )%7
+
 }
