9c43ee15779ddc5adc9e60a9ee43b0e91d8b0b18
olesigilai <ole_sigilai@yahoo.com>
2020-12-12 11:24:23 +0300

    adding contol flows

--- css/script.js
+++ css/script.js
@@ -1,22 +1,35 @@
-var maleName =["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw"]
-var femaleName =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-function getDay (){
+var maleName = ["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw"]
+var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+function getDay() {
 
 
-var year = document.getElementById("year").value ;
+    var year = document.getElementById("year").value;
+      
+    var mm = parseInt(document.getElementById("month").value);
+ 
+    var day = parseInt(document.getElementById("day").value);
+ 
+    var gender = document.getElementById("gender").value;
 
-var mm =parseInt(document.getElementById("month").value);
-
-var day = parseInt(document.getElementById("day").value);
-
-var gender = (document.getElementById("gender"));
+   alert(gender)
+    var yy = parseInt(year.substr(2, 4));
 
 
-var yy = parseInt(year.substr(2,4));
+    var century = parseInt(year.substr(0, 2));
 
-
-var century = parseInt(year.substr(0,2));
-
-var date = parseInt( ( (century/4) -2*century-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + day )%7
-
+    var date = parseInt(((century / 4) - 2 * century - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + day) % 7
+ alert(date)
+    if (mm <= 0 || mm > 12) {
+        alert("enter valid month")
+    } else
+    if (day < 1 || day > 31) {
+        alert("enter valid date")
+    } else
+    if (gender === "male") {
+        alert("your Akan male name is" + ""+ "" + maleName[date])
+    } else
+    if (gender === "female") {
+        alert("your Akan female name is" + "" + ""+ femaleName[date])
+    }
 }
